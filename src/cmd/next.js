0f855162a07ac38f8730a3c90d3cff9a6647f2ea
Ganesh Varadarajan <ganesh@coriolis.co.in>
2014-08-08 12:56:11 +0530

    `next` without arguments reads next object from stdin.

--- src/cmd/next.js
+++ src/cmd/next.js
@@ -14,7 +14,7 @@ inherit(Next, Command);
 Next.prototype.usage = 'next         -- get next item from pipeline\n\n' +
     'Usage:\n' +
     '    next -h | --help\n' +
-    '    next [-o <opts>] <obj>\n\n' +
+    '    next [-o <opts>] [<obj>]\n\n' +
     'Options:\n' +
     '    -h --help    Show this message.\n' +
     '    -o <opts>    Options to pass to lower layers\n';
@@ -22,7 +22,7 @@ Next.prototype.usage = 'next         -- get next item from pipeline\n\n' +
 Next.prototype.next = check_next(do_docopt(function() {
     var self = this,
         cliopts = optstr_parse(self.docopts['-o']),
-        obj = self.docopts['<obj>'];
+        obj = self.docopts['<obj>'] || self.fds.stdin;
 
     if (typeof obj.next !== 'function') {
         return self.exit("Object has no next() method");
@@ -34,7 +34,8 @@ Next.prototype.next = check_next(do_docopt(function() {
             return self.exit();
         }
         self.done = true;
-        return self.output(res);
+        var ret = res instanceof Array ? res[0] : res;
+        return self.output(ret);
     }));
 }));
 
