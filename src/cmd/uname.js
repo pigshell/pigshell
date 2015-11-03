46a4da8f620e6d2d9fdb19169dc83bdf29eccc19
Ganesh Varadarajan <ganesh@coriolis.co.in>
2015-11-03 19:26:49 +0530

    uname prints site name and url

--- src/cmd/uname.js
+++ src/cmd/uname.js
@@ -14,9 +14,16 @@ inherit(Uname, Command);
 Uname.prototype.usage = 'uname        -- print operating system name\n\n' +
     'Usage:\n' +
     '    uname [-arsv]\n' +
+    '    uname -n | -u\n' +
     '    uname -h | --help\n\n' +
     'Options:\n' +
-    '    -h --help    Show this message.\n';
+    '    -h --help    Show this message.\n' +
+    '    -a           Behave as if all the options -rsv were specified.\n' +
+    '    -r           Print operating system release.\n' +
+    '    -s           Print operating system name.\n' +
+    '    -v           Print git commit id of deployment.\n' +
+    '    -n           Print site name.\n' +
+    '    -u           Print site URL.\n';
 
 Uname.prototype.next = check_next(do_docopt(function() {
     var self = this,
@@ -25,7 +32,11 @@ Uname.prototype.next = check_next(do_docopt(function() {
 
     self.done = true;
 
-    if (self.docopts['-a']) {
+    if (self.docopts["-n"]) {
+        comps = [pigshell.site.name];
+    } else if (self.docopts["-u"]) {
+        comps = [pigshell.site.url];
+    } else if (self.docopts['-a']) {
         comps = ['Pigshell', pv.str, pv.git];
     } else {
         if (self.docopts['-s']) {
